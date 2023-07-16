97dbfdd1497cbeb5ff01beceb1d4c9dfb7276b56
Grzegorz Sawinski <sawinskigrzesiu@gmail.com>
2023-07-16 15:25:17 +0200

    fetching data + some style responsivnes added

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ const API_URL = 'https://api.geoapify.com/v1/geocode/autocomplete?text='
 const app = () =>{
     const inputSearch = document.querySelector('.search-bar input')
     const searchDiv = document.querySelector('.autocompletion-container')
+    const confirmBtn = document.querySelector('.search-bar button')
+    const hour = document.querySelector('.hour h1')
+    const minute = document.querySelector('.minute h1')
+    const second = document.querySelector('.second h1')
     let availableCities = []
     let timeoutId;
-    
+    let utf = 0
+
+    setInterval(()=>{
+        let hours = new Date().getHours() - 1 + utf
+        hours = hours >= 24 ? hours - 24 : hours 
+        hour.textContent = hours.toString().length === 2 ? hours : `0${hours}`
+        const minutes = new Date().getMinutes()
+        minute.textContent = minutes.toString().length === 2 ? minutes : `0${minutes}` 
+        const seconds = new Date().getSeconds()
+        second.textContent = seconds.toString().length === 2 ? seconds : `0${seconds}` 
+    }, 1000)
+
     const fetchCities = async(searchText) =>{
         try{
             if (searchText.length > 0){
@@ -22,7 +37,7 @@ const app = () =>{
 
                 //Fetch New Data
                 availableCities = [...new Set(data.features.map((city) =>{
-                    return city.properties.city
+                    return {city: city.properties.city, timezone: city.properties.timezone.offset_STD, code: city.properties.country_code}
                 }).filter(city => city !== undefined).slice(0,4))]
                 
                 const event = new CustomEvent('citiesChanged');
@@ -51,9 +66,9 @@ const app = () =>{
           availableCities.forEach(city =>{
             const autocompleteElement = document.createElement('button')
             autocompleteElement.classList.add('autocomplete-btn')
-            autocompleteElement.textContent = city
+            autocompleteElement.textContent = `${city.city}, ${city.code.toUpperCase()}`
             autocompleteElement.addEventListener('click', ()=>{
-                inputSearch.value = city
+                inputSearch.value = city.city
                 while(searchDiv.querySelector('.autocomplete-btn')){
                     searchDiv.removeChild(searchDiv.querySelector('.autocomplete-btn'))
                 }
@@ -62,6 +77,22 @@ const app = () =>{
         })
         }, 400); // Delay in milliseconds
       });
+
+      confirmBtn.addEventListener('click', async function(){
+        const title = document.querySelectorAll('.header-container h1')
+        await fetchCities(inputSearch.value)
+
+        if (availableCities.length > 0){
+            const cityObj = availableCities.filter(city => city.city.toLowerCase() === inputSearch.value.toLowerCase())
+            console.log(cityObj)
+            title[1].textContent = cityObj[0].city
+            utf = parseInt(cityObj[0].timezone.slice(0,3))
+            availableCities = []
+            inputSearch.value = ""
+        } else {
+
+        }
+      })
 
 }
 
